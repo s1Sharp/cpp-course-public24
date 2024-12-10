3876313e10c34fe47cd7877da71b56fc107aa27f
c0nf1dxnt <c0nf1dxnt@yandex.ru>
2024-12-10 16:59:56 +0300

    sinus_taylor was completed.

---
+++ lectures/lecture3-4/homework/main.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <iostream>
+#include "sinus_taylor.hpp"
+
+int main() {
+    sinus_taylor::print_sin(std::sin((double)1));
+    sinus_taylor::print_sin(sinus_taylor::sin((float)1));
+    sinus_taylor::print_sin(sinus_taylor::sin((double)1));
+    sinus_taylor::print_sin(sinus_taylor::sin((long double)1));
+    
+    return 0;
+}
