ee3ed9ce8f5a5272fc9df3f259b75d4c5831bf1b
RomanRakov <144253949+RomanRakov@users.noreply.github.com>
2025-01-07 09:18:12 +0000

    homework_by_Rakov

--- tasks/task1/list.cpp
+++ tasks/task1/list.cpp
@@ -3,21 +3,24 @@
 
 List::List()
 {
-
+    first = nullptr;
+    last = nullptr;
 }
 
 
 bool
 List::is_empty()
 {
-
+    return first == nullptr;
 }
 
 
 void
 List::clear()
 {
-
+    while (first) {
+        remove_back();
+    }
 }
 
 
@@ -25,40 +28,111 @@ List::clear()
 void
 List::push_front(int _val)
 {
-
+    Node* value = new Node(_val);
+    if (first == nullptr){
+        first = last = value;
+    } else {
+        value->next = first;
+        first = value;
+    }
 }
 
 
 void
 List::push_back(int _val)
 {
-
+    Node* value = new Node(_val);
+    if (first == nullptr){
+        first = last = value;
+    } else {
+        last->next = value;
+        last = value;
+    }
 }
 
 
 Node*
 List::find(int _val)
 {
-
+    Node* crnt = first;
+    while (crnt != nullptr){
+        if (crnt->val == _val){
+            return crnt;
+        }
+        crnt = crnt->next;
+    }
+     return nullptr;
 }
 
 
 void
 List::remove_front()
 {
-
+    if (first == nullptr){
+        last = nullptr;
+        return;
+    }else{
+        Node* tmp = first;
+        first = first->next;
+        delete tmp;
+    }   
 }
 
 
 void
 List::remove_back()
 {
-
+    if (first == nullptr){
+        last = nullptr;
+        return;
+    } else if (first == last){
+        delete first;
+        first = last = nullptr;
+        return;
+    } 
+    Node* val = first;
+    while (val->next != last) {
+        val = val->next;
+    }
+    delete last;            
+    last = val;   
+    last->next = nullptr;  
 }
 
 
 bool
 List::remove(const Node* _node)
 {
+    if (first == nullptr) {
+        return false;
+    } else if (first == _node)
+    {
+        Node* tmp = first;
+        first = first->next;
+        delete tmp;
 
+        if (first == nullptr) {
+            last = nullptr;
+        }
+
+        return true;
+    }
+    
+    Node* currentValue = first;
+    while (currentValue->next != nullptr) {
+        if (currentValue->next == _node) {
+            Node* tmp = currentValue->next;
+            currentValue->next = currentValue->next->next;
+
+            if (tmp == last) {
+                last = currentValue;
+            }
+
+            delete tmp;
+            return true;
+        }
+        currentValue = currentValue->next;
+    }
+
+    return false;
 }
