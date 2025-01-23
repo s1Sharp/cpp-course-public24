1f942f61d13476f439d0108d9dca7ff30aed80d3
range4 <143956987+range4@users.noreply.github.com>
2025-01-23 19:06:01 +0000

    Homework

---
+++ hw5taylor/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include "sinus_taylor.hpp"
+
+int main() {
+    sinus_taylor::print_sin((double)1);
+    sinus_taylor::print_sin((float)1);
+    sinus_taylor::print_sin((double)1);
+    sinus_taylor::print_sin((long double)1);
+    return 0;
+}
---
+++ hw5taylor/sinus_taylor.hpp
@@ -0,0 +1,15 @@
+#ifndef SINUS_TAYLOR_HPP
+#define SINUS_TAYLOR_HPP
+
+namespace sinus_taylor {
+    float sin(float x);
+    double sin(double x);
+    long double sin(long double x);
+    
+    void print_sin(float x);
+    void print_sin(double x);
+    void print_sin(long double x);
+}
+
+#endif
+
