0be209207bdbcb79b84aea6856a18844be06f072
NeesH73 <144137791+NeesH73@users.noreply.github.com>
2025-01-28 19:16:03 +0300

    1

--- tasks/task3/bin_search.hpp
+++ tasks/task3/bin_search.hpp
@@ -4,12 +4,12 @@ int* LowerBound(int* first, int* last, int value) {
 	while (first < last) {
 		int* mid = first + (last - first) / 2;
 		if (*mid < value) {
-            first = mid + 1;  
-        } 
+            		first = mid + 1;  
+        	} 
 		else {
 			last = mid; 
-        }
-    }
+        	}
+    	}
 	return first;
 }
 
