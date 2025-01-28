4f22dd429ceb5fdc8a9d3e03a8ee1aaa0f9df4db
BekerDi <143925720+BekerDi@users.noreply.github.com>
2025-01-28 22:02:45 +0000

    HW3

--- tasks/task3/bin_search.hpp
+++ tasks/task3/bin_search.hpp
@@ -1,5 +1,23 @@
 #pragma once
 
 int* LowerBound(int* first, int* last, int value) {
-	throw std::runtime_error("not implemented error");
+    if (first == nullptr || last == nullptr || first >= last) {
+        return last;
+    }
+
+    int* left = first; 
+    int* right = last;  
+
+    while (left < right) {
+        int* mid = left + (right - left) / 2; 
+        if (*mid < value) {
+            
+            left = mid + 1;
+        } else {
+            
+            right = mid;
+        }
+    }
+       
+    return left;
 }
--- tasks/task3/main.cpp
+++ tasks/task3/main.cpp
@@ -1,11 +1,34 @@
 #include "bin_search.hpp"
+#include <iostream>
 
-#include "iostream"
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int* first = arr;
+    int* last = arr + 5;
 
-int main()
-{
-    // nullptr - не должны падать, вернуть nullptr
-    // ТУТ ЗАГЛУШКА, ЧТОБЫ КОД КОМПИЛИРОВАЛСЯ
-    LowerBound(nullptr, nullptr, 0);
+    
+    int* result1 = LowerBound(first, last, 5);
+    std::cout << "LowerBound(5): " << (result1 - first) << " (value: " << *result1 << ")\n";
+
+    
+    int* result2 = LowerBound(first, last, 4);
+    std::cout << "LowerBound(4): " << (result2 - first) << " (value: " << *result2 << ")\n";
+
+    
+    int* result3 = LowerBound(first, last, 0);
+    std::cout << "LowerBound(0): " << (result3 - first) << " (value: " << *result3 << ")\n";
+
+    
+    int* result4 = LowerBound(first, last, 10);
+    if (result4 == last) {
+        std::cout << "LowerBound(10): out of range (returned last)\n";
+    }
+
+    
+    int* result5 = LowerBound(nullptr, nullptr, 10);
+    if (result5 == nullptr) {
+        std::cout << "LowerBound(nullptr): returned nullptr\n";
+    }
+
     return 0;
 }
