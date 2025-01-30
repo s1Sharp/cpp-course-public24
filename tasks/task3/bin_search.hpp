39fa286ecf4bb8d23d989a140ca1a7614de99d74
KelmurzinDanil <143899591+KelmurzinDanil@users.noreply.github.com>
2025-01-30 15:40:17 +0000

    All

--- tasks/task3/bin_search.hpp
+++ tasks/task3/bin_search.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
 int* LowerBound(int* first, int* last, int value) {
-	throw std::runtime_error("not implemented error");
+	int* left = first;
+    int* right = last;
+    while (left < right) {
+        int* mid = left + (right - left) / 2;
+        if (*mid < value) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return left;
 }
